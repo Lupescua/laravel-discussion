af019f5d3c334f76fb766a43f991f5ef2a8ddbd2
Jiřina Trojánková <jirina.trojankova@seznam.cz>
2018-02-26 13:57:17 +0100

    Views and urls

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/questions/{id}', 'QuestionController@show');
-
 Route::get('/questions', 'QuestionController@index');
+Route::get('/questions/{id}', 'QuestionController@show')->name('show question');
+
 
